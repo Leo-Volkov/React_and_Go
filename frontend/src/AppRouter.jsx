8007e92349f318d99c4a523a00bc81f60e795896
Лев <Leo.Volkov156@yandex.ru>
2024-09-17 19:46:33 +0300

    Update AppRouter.jsx

--- frontend/src/AppRouter.jsx
+++ frontend/src/AppRouter.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { BrowserRouter, Link, Route, Routes, Switch, Redirect } from 'react-router-dom'
-
 import About from './pages/About'
 import Posts from './pages/Posts'
 import PostIdPage from './pages/PostIdPage'
